abe4cb120a1f98af45ec95857a3068e18e39e431
israel Carlos <israelcarlos01@gmail.com>
2020-07-07 20:18:14 -0300

    cadastro de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,21 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+// importação para testar a rota
+// import User from './app/models/User';
 
 const routes = new Router();
+// essa rota foi para testar, para ver se estava criando Usuário...
+/* routes.get('/', async (req, res) => {
+  // sempre que for usar o await a funcão deve ser assíncrona
+  const user = await User.create({
+    name: 'Israel Carlos',
+    email: 'israel@gmail.com',
+    password_hash: '12312415325251',
+  });
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+  res.json(user);
+}); */
+
+routes.post('/users', UserController.store);
 
 export default routes;
