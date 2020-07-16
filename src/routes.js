e0fac40a506f8b250860ae139981f68079208eae
israel Carlos <israelcarlos01@gmail.com>
2020-07-16 20:52:06 -0300

    notifications

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,10 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import NotificationController from './app/controllers/NotificationController';
 
 import authMiddleware from './app/middlewares/auth';
+import ScheduleController from './app/controllers/ScheduleController';
 // importação para testar a rota
 // import User from './app/models/User';
 
@@ -35,8 +37,13 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
-routes.post('/files', upload.single('file'), FileController.store);
-
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
 export default routes;
